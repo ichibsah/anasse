a6240388d95a9d00b887a76b874718a7b700ace2
ibrahim <ibrahim@virthost01.skyline.lan>
2023-05-14 08:37:44 +0200

    Sun May 14 08:37:44 CEST 2023

--- git.sh
+++ git.sh
@@ -18,9 +18,10 @@
 
 git add .
 git commit -am "$(date)"
+git push
 
 #…or push an existing repository from the command line
-git remote add origin git@github.com:ichibsah/anasse.git
-git branch -M main
-git push -u origin main
+#git remote add origin git@github.com:ichibsah/anasse.git
+#git branch -M main
+#git push -u origin main
 
