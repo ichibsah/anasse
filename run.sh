146d2d99313baafde0d3dbfd99af6f0daa77ae18
ibrahim <ibrahim@de-1005-gw.skyline.lan>
2024-03-22 12:33:31 +0100

    Fri Mar 22 12:33:31 CET 2024

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #docker build -t anasse . 
 #docker run -it -d -p 8090:80 -v /home/ibrahim/sandbox/meryem/logs:/var/log/nginx:rw --name meryem meryem 
-#docker run -it -d -p 8090:80 --restart always -v /home/ibrahim/sandbox/anasse/website:/www -v /home/ibrahim/sandbox/anasse/logs:/var/log/nginx:rw --name anasse anasse 
+docker run -it -d -p 8090:80 --restart always -v ./website:/www -v ~home/sandbox/anasse/logs:/var/log/nginx:rw --name anasse anasse 
 #docker run -it meryem 
 #docker run -it node
 #docker exec -it meryem sh
