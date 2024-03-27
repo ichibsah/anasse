c3c58d9ee3606195bae7bd1464548b0364d0993d
ibrahim <ibrahim@de-1005-gw.skyline.lan>
2024-03-27 17:50:04 +0100

    Wed Mar 27 17:50:04 CET 2024

--- git.sh
+++ git.sh
@@ -19,7 +19,7 @@
 git add .
 git commit -am "$(date)"
 git push
-git push master master -f
+git push main main -f
 
 #…or push an existing repository from the command line
 #git remote add origin git@github.com:ichibsah/anasse.git
